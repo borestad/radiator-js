fbe5939f64335bfd946413a68b60099574524da4
Johan Borestad <johan.borestad@klarna.com>
2014-10-24 15:57:30 +0200

    Update test

--- test/test.js
+++ test/test.js
@@ -2,33 +2,14 @@
 
 var radiator = require('../lib/radiator.js');
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-
 exports.radiator = {
   setUp: function(done) {
     // setup here
     done();
   },
 
-  'no args': function(test) {
+  'Should warm my frozen soul': function(test) {
+    // TODO: Potentially refactor this scientificly perfect test
     test.ok(true);
     test.done();
   },
